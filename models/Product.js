321ce2308410dfa83ff980f5dd5a10578c0c2e5a
Alice Bryer <alicebryer123@gmail.com>
2022-07-11 15:13:55 +0100

    defined columns in models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // validate:  TO DO
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // default value:10,
+      // validate:
+    },
+    category_id: {
+      type: Datatypes.INTEGER,
+      // References category model's id.
+    },
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // Reference product models id
+    },
+    tag_id: {
+      type: Datatypes.INTEGER,
+      // Reference the tag models id
+    },
   },
   {
     sequelize,
